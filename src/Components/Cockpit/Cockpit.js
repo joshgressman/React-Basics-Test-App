b23a44c0f88861bd8dc7ac68c44a716a4b10c091
joshgressman <joshgressman@gmail.com>
2018-06-17 17:38:32 -0500

    added aux container

--- src/Components/Cockpit/Cockpit.js
+++ src/Components/Cockpit/Cockpit.js
@@ -1,33 +1,32 @@
-import React from 'react';
-import myClasses from './Cockpit.css';
+import React from "react";
+import myClasses from "./Cockpit.css";
+import Aux from "../../hoc/Aux";
 
-const cockpit = (props) => {
+const cockpit = props => {
+  const assignedClasses = [];
+  let btnClass = myClasses.Button;
 
-    const assignedClasses = [];
-    let btnClass = '';
-   
-    if(props.showPerson) {
-       btnClass = myClasses.Red;
-    }
+  if (props.showPerson) {
+    btnClass = [myClasses.Button, myClasses.Red].join(" ");
+  }
 
-    if(props.persons.length <= 2){
-      assignedClasses.push(myClasses.red);
-    }
+  if (props.persons.length <= 2) {
+    assignedClasses.push(myClasses.red);
+  }
 
-    if(props.persons.length <= 1){
-      assignedClasses.push(myClasses.bold);
-    }
- 
-    return (
-        <div className={myClasses.Cockpit}>
-        <h1>{props.appTitle}</h1>
-        <p className={assignedClasses.join(' ')}>Is this working</p>
-        <button className={btnClass}
-        onClick={props.clicked}
-        >Switch Name</button>
-        </div>
-    );
+  if (props.persons.length <= 1) {
+    assignedClasses.push(myClasses.bold);
+  }
+
+  return (
+    <Aux>
+      <h1>{props.appTitle}</h1>
+      <p className={assignedClasses.join(" ")}>Is this working</p>
+      <button className={btnClass} onClick={props.clicked}>
+        Switch Name
+      </button>
+    </Aux>
+  );
 };
 
-
-export default cockpit
+export default cockpit;
